d4bbf76780f07a9abebe50ed78c638d3b8817ea6
Ta-Chung Chen <chenalbert35@gmail.com>
2020-03-21 10:26:15 -0700

    Update

--- models/ingredients.js
+++ models/ingredients.js
@@ -1,7 +1,5 @@
 module.exports = function(sequelize, DataTypes) {
-    // Add code here to create a Post model
-    // This model needs a title, a body, and a category
-    // Don't forget to 'return' the post after defining
+
     var Ingredients = sequelize.define("Ingredients", {
       title: {
         type: DataTypes.STRING,
