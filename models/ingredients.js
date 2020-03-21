34b414ed20b2b14b56ad06cc15cf3f99e7a84cff
Ta-Chung Chen <chenalbert35@gmail.com>
2020-03-21 09:57:31 -0700

    Created Ingredients.js to create a table for ingredients list.

---
+++ models/ingredients.js
@@ -0,0 +1,32 @@
+module.exports = function(sequelize, DataTypes) {
+    // Add code here to create a Post model
+    // This model needs a title, a body, and a category
+    // Don't forget to 'return' the post after defining
+    var Ingredients = sequelize.define("Ingredients", {
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        notEmpty: true,
+        validate:{
+          len: [1, 160],
+        }
+      },
+      category: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        notEmpty: true
+      },
+      quantity:{
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        notEmpty: true,
+      },
+      select:{
+          type: DataTypes.BOOLEAN,
+          allowNull: false,
+          defaultValue: true
+      }
+    });
+    return Ingredients;
+  };
+  
