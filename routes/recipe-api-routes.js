65573886cb6542f769d77b0c7f8b5c3873ff1162
Ta-Chung Chen <chenalbert35@gmail.com>
2020-03-22 11:52:58 -0700

    Created api route file for recipe.

---
+++ routes/recipe-api-routes.js
@@ -0,0 +1,43 @@
+var db = require("../models");
+var isAuthenticated = require("../config/middleware/isAuthenticated");
+
+module.exports = function(app) {
+  app.get("/api/recipe", isAuthenticated, function(req, res) {
+    db.ingredients.findAll({}).then(function(dbIngred) {
+      res.json(dbIngred);
+    });
+  });
+
+  //   returns single recipe
+  app.get("/api/recipe/:id", isAuthenticated, function(req, res) {
+    db.ingredients
+      .findOne({
+        where: {
+          id: req.params.id
+        }
+      })
+      .then(function(dbIngred) {
+        res.json(dbIngred);
+      });
+  });
+
+  //   updates recipe to database
+  app.post("api/recipe", isAuthenticated, function(req, res) {
+    db.ingredients.create(req.body).then(function(dbIngred) {
+      res.json(dbIngred);
+    });
+  });
+
+  //   deletes single recipe
+  app.delete("/api/recipe/:id", isAuthenticated, function(req, res) {
+    db.ingredients
+      .destroy({
+        where: {
+          id: req.params.id
+        }
+      })
+      .then(function(dbIngred) {
+        res.json(dbIngred);
+      });
+  });
+};
