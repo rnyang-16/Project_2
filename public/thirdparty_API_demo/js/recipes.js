8f2583748041fc4c828063a30062f062b795dae5
Ruonan Y <joyceyang0730@gmail.com>
2020-03-22 18:49:45 -0700

    remove auto-complete function file

--- public/thirdparty_API_demo/js/recipes.js
+++ public/thirdparty_API_demo/js/recipes.js
@@ -1,43 +1,41 @@
-$('input[name="ingredients"]').amsifySuggestags({
-    suggestions: ['cheese', 'chicken', 'beef', 'fish', 'butter', 'salt'],
-    showAllSuggestions: false
-});
+// $("#ingredients").amsifySuggestags({
+//     suggestions: ['cheese', 'chicken', 'beef', 'fish', 'butter', 'salt'],
+//     showAllSuggestions: false
+// });
 
 $("#search_recipes").on("click", function() {
-    event.preventDefault();
-    console.log($("input[name=ingredients]").val());
-    var queryURL = buildQueryURL();
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(updatePage);
-
+  event.preventDefault();
+  console.log($("#ingredients").val());
+  var queryURL = buildQueryURL();
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(updatePage);
 });
 
 function updatePage(RecipeData) {
-    // Get from the form the number of results to display
-    console.log(RecipeData);
-    // TODO: fill in the page with RecipeData
+  // Get from the form the number of results to display
+  console.log(RecipeData);
+  // TODO: fill in the page with RecipeData
 }
 
-
 function buildQueryURL() {
-    // queryURL is the url we'll use to query the API
-    var queryURL = "https://api.spoonacular.com/recipes/findByIngredients?";
+  // queryURL is the url we'll use to query the API
+  var queryURL = "https://api.spoonacular.com/recipes/findByIngredients?";
 
-    // Begin building an object to contain our API call's query parameters
-    // Set the API key
-    var queryParams = { "apiKey": "464c61bf28e446659ccf514ecb520d77" };
+  // Begin building an object to contain our API call's query parameters
+  // Set the API key
+  var queryParams = { apiKey: "464c61bf28e446659ccf514ecb520d77" };
 
-    // Grab text the user typed into the search input, add to the queryParams object
-    queryParams.ingredients = $("input[name=ingredients]")
-        .val()
-        .trim();
+  // Grab text the user typed into the search input, add to the queryParams object
+  queryParams.ingredients = $("#ingredients")
+    .val()
+    .trim();
 
-    // The maximum number of recipes to return
-    queryParams.number = 3;
+  // The maximum number of recipes to return
+  queryParams.number = 3;
 
-    // Logging the URL so we have access to it for troubleshooting
-    console.log(queryURL + $.param(queryParams));
-    return queryURL + $.param(queryParams);
+  // Logging the URL so we have access to it for troubleshooting
+  console.log(queryURL + $.param(queryParams));
+  return queryURL + $.param(queryParams);
 }
